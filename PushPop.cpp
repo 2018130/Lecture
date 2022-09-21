0d1cdc8c691b55bebcfeca6acc748e34e9235ad8
2018130 <35947698+2018130@users.noreply.github.com>
2022-09-21 20:41:05 +0900

    Update PushPop.cpp

--- PushPop.cpp
+++ PushPop.cpp
@@ -4,22 +4,23 @@
 
 struct Node* head = NULL;
 
-struct Node{
+struct Node {
 	char data;
 	Node* nextNode;
 };
-	
-void push(char data){
+
+void push(char data) {
 	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-	newNode->	data = data;
+	newNode->data = data;
 	newNode->nextNode = NULL;
-	
-	if(head == NULL) {
+
+	if (head == NULL) {
 		head = newNode;
-	}else{
+	}
+	else {
 		Node* curNode = head;
-		while(true){
-			if(curNode->nextNode == NULL){
+		while (true) {
+			if (curNode->nextNode == NULL) {
 				curNode->nextNode = newNode;
 				break;
 			}
@@ -28,42 +29,43 @@ void push(char data){
 	}
 }
 
-char pop(void){
-	if(head == NULL) return '-';
-	
-	Node* preLastNode;
-	Node* lastNode;
+char pop(void) {
+	if (head == NULL) return '-';
+
+	Node* preLastNode = NULL;
+	Node* lastNode = NULL;
 	Node* curNode = head;
-	
-	while(curNode != NULL){
+
+	while (curNode != NULL) {
 		preLastNode = lastNode;
 		lastNode = curNode;
-		curNode = curNode->	nextNode;
+		curNode = curNode->nextNode;
 	}
 	int lastData = lastNode->data;
-	
-	preLastNode->nextNode = NULL;
-	if(lastNode == head) head = NULL;
+
+	if(preLastNode != NULL)
+		preLastNode->nextNode = NULL;
+	if (lastNode == head) 
+		head = NULL;
 	free(lastNode);
 	lastNode = NULL;
-	
+
 	return lastData;
 }
 
-int main(void){
+int main(void) {
 	int count = 0;
 	char key;
-	
-	do{
+
+	do {
 		count++;
-		printf("%2d 번 문자 입력>", count);
-		key = getch();
+		printf("%2d => ", count);
+		key = _getch();
 		push(key);
 		printf("%c \n", key);
-	}while(key != 27);
+	} while (key != '!');
 	printf("\n\n");
-	printf("데이터 pop과정\n\n");
-	while(count != 0){
+	while (count != 0) {
 		key = pop();
 		count--;
 		printf("%c", key);
